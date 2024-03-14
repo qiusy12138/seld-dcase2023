004bec08711da748f3f299f3df57bf3c8443dca4
qiusy12138 <154852391+qiusy12138@users.noreply.github.com>
2024-03-14 16:46:45 +0800

    Update batch_feature_extraction.py

--- batch_feature_extraction.py
+++ batch_feature_extraction.py
@@ -1,28 +1,28 @@
 # Extracts the features, labels, and normalizes the development and evaluation split features.
-# 提取特征、标签并规范化开发和评估分割特征。
+# 提取特征、标签并规范化开发和评估拆分特征。
 
-# 加载项目中的其他文件特征类别cls_feature_class和参数parameters
+# 加载项目中的其他文件：特征类别cls_feature_class和参数parameters
 import cls_feature_class
 import parameters
 # 加载Python中自带的工具包sys
 import sys
 
 
-def main(argv):
+def main(argv):            # argv保存命令行输入的参数值,argv[0]指向程序自身运行目录路径和程序名，,argv[1]指向程序在DOS命令中执行程序名后的第一个字符串,argv[2]指向第二个字符串
     # Expects one input - task-id - corresponding to the configuration given in the parameter.py file.
     # 需要一个输入：task-id（任务-id）来对应于parameter.py文件中给定的配置。
     # Extracts features and labels relevant for the task-id
-    # 提取与task-id（任务-id）相关的功能和标签
+    # 提取与task-id（任务-id）相关的特征和标签
     # It is enough to compute the feature and labels once. 
     # 计算一次特征和标签就足够了。
 
     # use parameter set defined by user
-    # 使用用户定义的参数集
+    # 使用用户定义的参数集，这里的参数集是给定的数据集
     task_id = '1' if len(argv) < 2 else argv[1]
     params = parameters.get_params(task_id)
 
     # -------------- Extract features and labels for development set -----------------------------
-    # -------------- 提取开发集的功能和标签 -----------------------------
+    # -------------- 提取开发集（给定的数据集）的特征和标签 -----------------------------
     dev_feat_cls = cls_feature_class.FeatureClass(params)
 
     # # Extract features and normalize them
